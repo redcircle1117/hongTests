ebf1eecba478b6ad2e8f2a120d455c26ebdc9357
redcircle1117 <775161932@qq.com>
2015-12-14 22:59:52 +0800

    测试github初始化提交

---
+++ app/src/main/java/com/example/hong/asdsd/MainActivity.java
@@ -0,0 +1,67 @@
+package com.example.hong.asdsd;
+
+import android.app.Activity;
+import android.app.Notification;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.graphics.Color;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.logging.Filter;
+
+public class MainActivity extends Activity  {
+
+    private TextView tv_main_q;
+    private TextView tv_main;
+    private ImageButton ib_main;
+
+    MyReceiver2 receiver2 = new MyReceiver2();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tv_main = (TextView) findViewById(R.id.tv_main);
+        tv_main_q = (TextView) findViewById(R.id.tv_main_q);
+        ib_main = (ImageButton) findViewById(R.id.ib_main);
+
+        IntentFilter filter = new IntentFilter();
+        filter.addAction("android.net.conn.CONNECTIVITY_CHANGE");
+        registerReceiver(receiver2, filter);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(receiver2);
+    }
+
+
+    class MyReceiver2 extends BroadcastReceiver{
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+
+            if(activeNetworkInfo == null && activeNetworkInfo.isAvailable()){
+                Toast.makeText(context, "没有网络", Toast.LENGTH_SHORT).show();
+            }else{
+                Toast.makeText(context, "网络改变", Toast.LENGTH_SHORT).show();
+            }
+
+
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/hong/asdsd/MyReceiver.java
@@ -0,0 +1,15 @@
+package com.example.hong.asdsd;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by Hong on 2015/12/14.
+ */
+public class MyReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
